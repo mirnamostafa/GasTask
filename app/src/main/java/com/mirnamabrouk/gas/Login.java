bac887f4f915729fd86c5453dfea58d0286a62cd
mirnamostafa <31778554+mirnamostafa@users.noreply.github.com>
2017-09-09 03:26:04 +0200

    Initial commit

---
+++ app/src/main/java/com/mirnamabrouk/gas/Login.java
@@ -0,0 +1,16 @@
+package com.mirnamabrouk.gas;
+
+import android.graphics.Paint;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.Html;
+import android.widget.TextView;
+
+public class Login extends AppCompatActivity {
+    TextView create_account;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+         }
+}
